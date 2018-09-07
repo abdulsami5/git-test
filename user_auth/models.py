985551d24fa42d5d969a85b92d462dd3511f643a
Abdul Sami <abdul.sami@tintash.com>
2018-09-07 11:22:25 +0500

    Implement User Model
    Implement user model in user_auth/models.py to define user and make AUTH_USER_MODEL equal to this model in settings.py to use this custom model for authentication.

--- user_auth/models.py
+++ user_auth/models.py
@@ -1,8 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
-
+from django.contrib.auth.models import AbstractUser
+import uuid
 # Create your models here.
 
-class User(User):
+class User(AbstractUser):
     """This is the User model to represent User entity"""
-    pass
+    uuid = models.UUIDField(unique=True,default=uuid.uuid4, editable=False, primary_key=True)
+    profile_pic = models.CharField(blank=True, max_length=1000)
+
+    def __str__(self):
+        """This function is to get representation of User model"""
+        return self.email
