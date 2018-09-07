6a460ad79e8601e40b81a8503cdd694c5139b273
Abdul Sami <abdul.sami@tintash.com>
2018-09-07 11:25:54 +0500

    Implement UserSerializer
    Implement a serializer 'UserSerializer' for user model in user_auth/serializers.py.

---
+++ user_auth/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth.models import Group
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """serializer class for User model"""
+    groups = serializers.SerializerMethodField()
+    class Meta:
+        model = User
+        fields = ('uuid', 'username', 'email', 'profile_pic', 'password', 'groups', 'first_name', 'last_name')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+                email=validated_data['email'],
+                username=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def get_groups(self, obj):
+       return [group.name for group in obj.groups.all()]
